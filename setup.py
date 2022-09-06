0a1d61e8943500e685e1f3003d60ec4ec367bfa2
Matthew Cen <6173495+matthewcen@users.noreply.github.com>
2022-09-05 20:00:56 -0400

    bump dependency version and enforce minimum python version

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ import sys
 import libgrabsite
 
 install_requires = [
-	"click>=6.3",
+	"click>=8.0",
 	"wpull @ https://github.com/matthewcen/ludios_wpull/tarball/master",
-	"manhole>=1.0.0",
-	"lmdb>=0.89",
-	"autobahn>=0.12.1",
-	"fb-re2>=1.0.6",
-	"websockets>=6.0",
+	"manhole>=1.5.0",
+	"lmdb>=1.3.0",
+	"autobahn>=22.1.1",
+	"fb-re2>=1.0.7",
+	"websockets>=10.3",
 ]
 
 if 'GRAB_SITE_NO_CCHARDET' not in os.environ:
-	install_requires.append("cchardet>=1.0.0")
+	install_requires.append("cchardet>=2.1.7")
 
 setup(
 	name="grab-site",
@@ -39,5 +39,6 @@ setup(
 	scripts=["grab-site", "gs-server", "gs-dump-urls"],
 	packages=["libgrabsite"],
 	package_data={"libgrabsite": ["*.html", "*.ico", "*.txt", "ignore_sets/*"]},
+	python_requires=">3.8",
 	install_requires=install_requires,
 )
